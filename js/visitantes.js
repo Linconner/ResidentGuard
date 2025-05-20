3a6a0da7ce326cf3269b517ca8f0ed36cfea778f
Lincon <linconsegato32@gmail.com>
2025-05-19 22:46:04 -0300

    Tela de eventos + navbar completa

--- js/visitantes.js
+++ js/visitantes.js
@@ -16,3 +16,20 @@ radios.forEach(radio => {
     }
 });
 });
+
+const menu = document.getElementById('menu');
+const popup = document.getElementById('popup');
+const btnfechar = document.getElementById('btnfechar');
+
+menu.addEventListener('click', () => {
+  if (popup.style.display === 'none' || popup.style.display === '') {
+    // Se o popup está escondido, mostre-o
+    popup.style.display = 'flex'; // Ou 'block', dependendo do seu CSS
+  } else {
+    // Se o popup está visível, esconda-o
+    popup.style.display = 'none';
+  }
+});
+btnfechar.addEventListener('click', () => {
+    popup.style.display = 'none'; 
+})
