06e9367e9e86e6bc0dee96f7983bb90fe69c93d8
Lincon <linconsegato32@gmail.com>
2025-06-11 22:58:39 -0300

    alteraçãorapida

--- js/RegistroM.js
+++ js/RegistroM.js
@@ -1,17 +1,33 @@
-const checkboxs = document.querySelectorAll('input[type="checkbox"]');
+const checkboxs = document.querySelectorAll('input[type="checkbox"]:not(#naopossuo)');
 
 const carro = document.getElementById('carro');
 const moto = document.getElementById('moto');
 const outro = document.getElementById('outro');
+//esseaqui é o input (o de baixo)
 const outroinput = document.getElementById('outroinput');
+//a div containerextra
+const divoutroinput = document.getElementById('containeradicional')
+const inner = document.getElementById('inner');
+const naopossuo = document.getElementById('naopossuo');
 
 checkboxs.forEach((checkbox) => {
     checkbox.addEventListener('change', () => {
         if (checkbox.value === 'outro') {
-            outroinput.classList.toggle('ativo', checkbox.checked);
-        } else {
-            outroinput.classList.remove('ativo');
-        }
+           divoutroinput.classList.toggle('ativo', checkbox.checked);
+        } 
     })  
 })
 
+naopossuo.addEventListener ('change', function () {
+if (naopossuo.checked) {
+    checkboxs.forEach (cb => cb.checked = false);
+}
+});
+
+checkboxs.forEach(cb => {
+    cb.addEventListener('change', function () {
+        if (cb.checked) {
+            naopossuo.checked = false;
+        }
+    });
+});
