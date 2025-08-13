64c623bbf6d9bcd0bed4f5d1d929b26a12ac50f3
Linconner <linconsegato32@email.com>
2025-08-13 12:09:13 -0300

    Mudanças enormes em várias telas sobre responsividade e agrupamento de elementos. Ultima mudança nessas telas.

--- js/RegistroV.js
+++ js/RegistroV.js
@@ -2,10 +2,13 @@ const radiocarro = document.getElementById("carro");
 const radiomoto = document.getElementById("moto");
 const radiobicicleta = document.getElementById("bicicleta");
 const placa = document.getElementById("placa");
+const acessocomum = document.getElementById("acessocomum");
 
 document.addEventListener("change", function () {
   if (radiocarro.checked || radiomoto.checked) {
     placa.classList.add("placavisible");
+  } else if (acessocomum.checked || radiobicicleta.checked) {
+    placa.classList.remove("placavisible");
   } else {
     placa.classList.remove("placavisible");
   }
